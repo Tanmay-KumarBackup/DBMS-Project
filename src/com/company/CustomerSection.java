ea3955b7b9a6fb1613b6385bdaccec7bdbc36314
Tanmay-KumarBackup <tanmay.kumar2019@vitstudent.ac.in>
2020-10-16 09:58:56 +0530

    now i am able to run the program, mysql is yet to be included

---
+++ src/com/company/CustomerSection.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import javax.swing.*;
+
+public class CustomerSection extends JFrame{
+    private JButton modifyButton;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextArea textArea1;
+    private JButton insertValueButton;
+    private JComboBox comboBox1;
+    private JTextField textField4;
+    private JButton fetchDataButton;
+    private JPanel CustomerRoot;
+
+    public CustomerSection()
+    {
+        add(CustomerRoot);
+        setTitle("Customer Section");
+        setSize(400,500);
+
+
+    }
+
+}
