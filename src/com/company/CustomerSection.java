fd6ada0f7aa06f4c34b854623ed6bc3596bd8a78
tanmay <tanmay.kumar2019@vitstudent.ac.in>
2020-10-20 15:48:51 +0530

    NULLPointerException Error is shown

--- src/com/company/CustomerSection.java
+++ src/com/company/CustomerSection.java
@@ -1,29 +1,28 @@
 package com.company;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+
 import java.sql.*;
 class MysqlCon{
     public MysqlCon(String sql) {
 
     }
 
-    public static void MysqlCon(String args){
+    public static void mysqlCon(String args){
         try{
             Class.forName("com.mysql.jdbc.Driver");
             Connection con=DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/Project_DBMS","root","TanuCherry1!");
-            //here Project_DBMS is database name, root is username and TanuCherry1! is the password
-            System.out.println("Connected database successfully...");
-            System.out.println("Inserting records into the table...");
-            Statement stmt=null;
+                    "jdbc:mysql://localhost:3306/Project_DBMS","root","0310");
+            //here Project_DBMS is database name, root is username and 0310 is the password
+            System.out.println("Connected database successfully...\n");
+            System.out.println("Inserting records into the table...\n");
+            Statement stmt;
             Connection conn=null;
             assert false;
             stmt = conn.createStatement();
 
             stmt.executeUpdate(args);
-            System.out.println("Inserted records into the table...");
+            System.out.println("Inserted records into the table...\n\n");
         }catch(Exception se){ System.out.println(se);}
 
     }
@@ -49,18 +48,16 @@ public class CustomerSection extends JFrame{
         setSize(750,300);
 
 
-        insertValueButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
+        insertValueButton.addActionListener(actionEvent -> {
 
-                String name=CName.getText();
-                String addr=Addr.getText();
-                int phno=Integer.parseInt(phNo.getText());
-                int c_id=Integer.parseInt(C_ID.getText());
-                String sql= "INSERT INTO Customer_details " +"VALUES ( "+c_id+","+CName+" , "+Addr+" , ";
-                new MysqlCon(sql);
+            String name=CName.getText();
+            String addr=Addr.getText();
+            int phone=Integer.parseInt(phNo.getText());
+            int c_id=Integer.parseInt(C_ID.getText());
+            String sql= "INSERT INTO Customer_details(customer_ID , name , PhNo , Address) " +"VALUES ( "+c_id+","+CName+" , "+phone+" , "+Addr+" );";
+            MysqlCon.mysqlCon(sql);
 
-            }
+
         });
     }
 
