7f5af9093f5a7df452ee91edd73ede837be0886a
tanmay <tanmay.kumar2019@vitstudent.ac.in>
2020-10-22 12:23:46 +0530

    CustomerSection.java Completely working

---
+++ src/com/company/Billing_Section.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Billing_Section {
+    private JPanel CustomerRoot;
+    private JTextField C_ID;
+    private JTextField BillDate;
+    private JTextField fValue;
+    private JComboBox comboBox1;
+    private JButton modifyButton;
+    private JButton insertValueButton;
+    private JButton fetchDataButton;
+    private JButton backToMainMenuButton;
+    private JTextField TotalAmt;
+    private JTextArea Details;
+}
--- src/com/company/CustomerSection.java
+++ src/com/company/CustomerSection.java
@@ -1,14 +1,10 @@
 package com.company;
 
-import com.mysql.cj.jdbc.MysqlDataSource;
+import javafx.util.converter.BigIntegerStringConverter;
+
 import javax.swing.*;
 import java.sql.*;
-import java.util.Properties;
-import java.io.FileInputStream;
-import java.io.IOException;
-
-
-
+import java.math.BigInteger;
 public class CustomerSection extends JFrame {
 
     private JButton modifyButton;
@@ -17,21 +13,20 @@ public class CustomerSection extends JFrame {
     private JTextField phNo;
     private JTextArea Addr;
     private JButton insertValueButton;
-    private JComboBox comboBox1;
-    private JTextField textField4;
+    private JComboBox<String> comboBox1;
+    private JTextField fValue;
     private JButton fetchDataButton;
     private JPanel CustomerRoot;
+    private JButton backToMainMenuButton;
 
-
-
-    public static int insertCandidate(int customer_ID, String name, int Phno,
+    public static int insertCandidate(int customer_ID, String name, BigInteger Phno,
                                       String address) {
         // for insert a new candidate
         ResultSet rs = null;
         int candidateId = 0;
 
         String sql = "INSERT INTO Customer_details(customer_ID , name , PhNo , Address) "
-                + "VALUES(?,?,?,?)";
+                + "VALUES(?,?,?,?) ;";
 
         try (Connection conn = MySQLJDBCUtil.getConnection();
              PreparedStatement pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);) {
@@ -63,7 +58,34 @@ public class CustomerSection extends JFrame {
         return candidateId;
     }
 
+    public static void fetch(int index, String name) {
+        String query = null;
+        if(index==0) {
+            query="SELECT * FROM Customer_details WHERE customer_ID = "+ Integer.parseInt(name)+";";
+        }
+        else if(index==1){
+            query="SELECT * FROM Customer_details WHERE name = "+ name+";";
+        }
+        else if(index==2){
+            query="SELECT * FROM Customer_details WHERE PhNo = "+ name+";";
+        }
+        ResultSet rs;
+        
+        try (Connection conn = MySQLJDBCUtil.getConnection();
+            CallableStatement stmt = conn.prepareCall(query)) {
+
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                System.out.print(" Customer Name is: "+rs.getString("name") + "\n Phone Number is: "+ rs.getInt("PhNo")+"\n Address is: "+ rs.getString("Address"));
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+
     public CustomerSection() {
+
         add(CustomerRoot);
         setTitle("Customer Section");
         setSize(750, 300);
@@ -73,12 +95,21 @@ public class CustomerSection extends JFrame {
 
             String name = CName.getText();
             String addr = Addr.getText();
-            int phone = Integer.parseInt(phNo.getText());
+            BigInteger phone = new BigInteger(phNo.getText());
             int c_id = Integer.parseInt(C_ID.getText());
             int id = insertCandidate(c_id, name, phone, addr);
 
             System.out.printf("A new candidate with id %d has been inserted.%n",id);
         });
-    }
 
+        comboBox1.addItem("Customer_ID");
+        comboBox1.addItem("Name");
+        comboBox1.addItem("Phone Number");
+
+
+        fetchDataButton.addActionListener(actionEvent -> {
+            String fVal = fValue.getText();
+            fetch(comboBox1.getSelectedIndex(),fVal);
+        });
+    }
 }
